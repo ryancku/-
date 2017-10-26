46e1b7be7d75ae66645dec5d23618b075d521eaa
ryancku <p76051276@mail.ncku.edu.tw>
2017-10-26 17:16:53 +0800

    Add files via upload

---
+++ decode_big5.py
@@ -0,0 +1,12 @@
+import requests
+from bs4 import BeautifulSoup
+
+res = requests.get('https://www.ptt.cc/bbs/Tech_Job/index.html')
+res.encoding = 'utf8'
+soup = BeautifulSoup(res.text, "html.parser")
+
+for i in soup.findAll('a'):
+    count += 1
+    if "½Ð¯q".decode('big5') in i.get_text():
+        print i, count
+
